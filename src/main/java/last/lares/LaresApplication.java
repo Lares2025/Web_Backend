932135b3f914677233fe137c0f54485ff6747976
wwwxsv19 <wwwxsv19@gmail.com>
2025-04-09 00:19:08 +0900

    set: Project

---
+++ src/main/java/last/lares/LaresApplication.java
@@ -0,0 +1,13 @@
+package last.lares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaresApplication.class, args);
+    }
+
+}
