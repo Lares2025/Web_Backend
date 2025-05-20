7b4eb1bd65ad093cc2b51f1d1119bc04ab839658
wwwxsv19 <wwwxsv19@gmail.com>
2025-05-20 20:08:52 +0900

    feat: 로봇 제어 내역 생성 구현

--- src/main/java/last/lares/domain/control/service/DataService.java
+++ src/main/java/last/lares/domain/control/service/DataService.java
@@ -5,6 +5,7 @@ import last.lares.domain.control.ControlData;
 import last.lares.domain.control.presentation.dto.DataListDto;
 import last.lares.domain.control.repository.DataRepository;
 import last.lares.domain.control.types.ControlType;
+import last.lares.domain.robot.Robot;
 import last.lares.domain.robot.repository.RobotRepository;
 import last.lares.domain.user.User;
 import last.lares.domain.user.repository.UserRepository;
@@ -13,11 +14,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class DataService {
+    private final UserRepository userRepository;
+    private final RobotRepository robotRepository;
     private final DataRepository dataRepository;
 
     @Transactional
@@ -38,6 +42,27 @@ public class DataService {
         return DataListDto.builder()
                 .dataInfoList(dataInfoList)
                 .build();
+    }
+
+    @Transactional
+    public void newData(String userId, int robotId, int controlAmount, ControlType controlDirection) {
+        // 추후 명확한 제어 요청 DTO ( ControlRequestDto ) 생성 시, 본 메서드 인자값 형태 대체 예정
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 사용자입니다 : " + userId));
+
+        Robot robot = robotRepository.findById(robotId)
+                .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 로봇입니다 : " + robotId));
+
+        ControlData controlData = ControlData.builder()
+                .controlCreatedAt(LocalDateTime.now())
+                .user(user)
+                .robot(robot)
+                .controlAmount(controlAmount)
+                .controlDirection(controlDirection)
+                .build();
+
+        dataRepository.save(controlData);
     }
 
     @Transactional
