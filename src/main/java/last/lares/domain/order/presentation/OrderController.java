928071aa6568ec3ec7b4d18b1af0854ec3b4ab1f
wwwxsv19 <wwwxsv19@gmail.com>
2025-05-21 11:18:37 +0900

    feat: 모든 배송 내역 조회 구현

--- src/main/java/last/lares/domain/order/presentation/OrderController.java
+++ src/main/java/last/lares/domain/order/presentation/OrderController.java
@@ -1,21 +1,34 @@
 package last.lares.domain.order.presentation;
 
 import last.lares.domain.order.presentation.dto.OrderCreateRequestDto;
+import last.lares.domain.order.presentation.dto.OrderListDto;
 import last.lares.domain.order.service.OrderService;
 import last.lares.global.dto.CommonResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order")
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderService orderService;
+
+    @GetMapping("/")
+    public ResponseEntity<?> allOrder() {
+        try {
+            OrderListDto orderListDto = orderService.allOrder();
+
+            return ResponseEntity.ok().body(orderListDto);
+        } catch (Exception e) {
+            CommonResponseDto response = CommonResponseDto.builder()
+                    .message("내부 서버 에러가 발생하였습니다.")
+                    .build();
+
+            return ResponseEntity.internalServerError().body(response);
+        }
+    }
 
     @PostMapping("/")
     public ResponseEntity<?> newOrder(@RequestBody OrderCreateRequestDto orderCreateRequestDto) {
--- src/main/java/last/lares/domain/order/service/OrderService.java
+++ src/main/java/last/lares/domain/order/service/OrderService.java
@@ -3,6 +3,7 @@ package last.lares.domain.order.service;
 import jakarta.transaction.Transactional;
 import last.lares.domain.order.Order;
 import last.lares.domain.order.presentation.dto.OrderCreateRequestDto;
+import last.lares.domain.order.presentation.dto.OrderListDto;
 import last.lares.domain.order.repository.OrderRepository;
 import last.lares.domain.user.User;
 import last.lares.domain.user.repository.UserRepository;
@@ -13,12 +14,35 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
     private final UserRepository userRepository;
     private final OrderRepository orderRepository;
+
+    @Transactional
+    public OrderListDto allOrder() {
+        List<Order> orderList = orderRepository.findAll();
+
+        List<OrderListDto.OrderInfo>  orderInfoList = orderList.stream()
+                .map(order -> OrderListDto.OrderInfo.builder()
+                        .orderId(order.getOrderId())
+                        .orderCreatedAt(order.getOrderCreatedAt())
+                        .sendUserId(order.getSendUser().getUserId())
+                        .receiveUserId(order.getReceiveUser().getUserId())
+                        .receiveUserAddress(order.getReceiveUser().getUserAddress())
+                        .orderItem(order.getOrderItem() + " / " + order.getOrderAmount())
+                        .orderMemo(order.getOrderMemo())
+                        .deliveryDate(order.getDeliveryDate())
+                        .build()
+                ).toList();
+
+        return OrderListDto.builder()
+                .orderInfoList(orderInfoList)
+                .build();
+    }
 
     @Transactional
     public CommonResponseDto newOrder(OrderCreateRequestDto request) {
